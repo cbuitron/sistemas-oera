ae528d66f68e2ea467a01fa627fd336dca8dd812
cbuitron <carlos.buitron.quispe@hotmail.com>
2019-03-22 01:48:06 -0500

    avance configuracion

---
+++ src/main/java/unalm/oera/AlmacenUnalm/AlmacenUnalmApplication.java
@@ -0,0 +1,13 @@
+package unalm.oera.AlmacenUnalm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenUnalmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenUnalmApplication.class, args);
+	}
+
+}
